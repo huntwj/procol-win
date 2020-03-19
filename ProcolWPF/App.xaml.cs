afe87427fff350a14688103dd2aeb4c7abc6a9ff
Wil Hunt <huntwj@gmail.com>
2020-03-19 01:22:35 -0700

    Initial commit

---
+++ ProcolWPF/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ProcolBridge;
+using System.Windows;
+
+namespace ProcolWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public Bridge Bridge { get; set; }
+
+        public App()
+        {
+            Bridge = new Bridge();
+        }
+    }
+}
